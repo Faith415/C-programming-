f56f205ea21a3f7705743f13e5d5746e13f4b30d
Faith415 <faithabuga415@gmail.com>
2025-09-25 14:03:24 -0700

    Add files via upload

---
+++ compound interest.c
@@ -0,0 +1,30 @@
+
+ /*
+Name: Faith Achieng 
+Reg no.: CT101/G/26530/25
+Description :Compoundinterest 
+*/
+#include <stdio.h>
+  int main()
+  {
+  	float principle,rate,time,compounding_years,compound_intrest,final_amount;
+  	printf("enter princple:");
+  	scanf("%f" ,&principle);
+  		
+  	printf("enter rate:");
+  	scanf("%f" ,&rate);
+  	
+  	printf("enter time:");
+  	scanf("%f" ,&time);
+  		
+  	printf("enter compounding_years:");
+  	scanf("%f" ,&compounding_years);
+  	
+  	final_amount = principle * pow((1+rate/100), time * compounding_years);
+  	
+  	compound_intrest =final_amount-principle;
+  	
+  	printf("your compound_intrest is ksh %.2f" ,compound_intrest);
+  	
+  	return 0;
+  }
