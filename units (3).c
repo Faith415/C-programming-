bb9551f11881d41512a7479baf82c300b25ab99e
Faith415 <faithabuga415@gmail.com>
2025-09-25 05:10:13 -0700

    Add files via upload

---
+++ units (3).c
@@ -0,0 +1,21 @@
+/*
+name:Faithe Achieng
+reg no:CT101/G/26530
+unit:programming
+description : list of units
+*/
+#include<stdio.h>
+
+int main (){
+printf("units");
+
+printf(" programming /n");
+printf(" analogue electronics /n");
+printf(" maths for science /n" );
+printf(" computer architecture /n ");
+printf(" physics /n");
+printf(" communication skills /n");
+printf(" discrete mathematics /n");
+printf(" ICT /n");
+return 0;
+}
