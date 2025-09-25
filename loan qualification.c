7f292c001a05705cd54787314bf6b2ae6143a8de
Faith415 <faithabuga415@gmail.com>
2025-09-25 13:32:06 -0700

    Add files via upload

---
+++ loan qualification.c
@@ -0,0 +1,22 @@
+/*
+name:Faithe Achieng
+reg no:CT101/G/26530
+unit:programming
+#include<studio.h>
+programme .loan qualifications 
+*/
+int main () {
+int age ;
+float income ;
+printf("welcome to our loan application  system \n");
+printf("please enter your age");
+scanf("%d",&age);
+
+printf("please enter your manual income (sh)");
+scanf("%f",&income);
+
+if(age >21&&income>=21000);
+printf("congratulations !yqualify for the loan /n");
+printf("unfortunately you don't qualify for the loan\n ");
+return 0 ;
+}
