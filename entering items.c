ed60f34c938a324511b82b54d07279313514ad6a
Faith415 <faithabuga415@gmail.com>
2025-09-25 06:40:52 -0700

    Add files via upload

---
+++ entering items.c
@@ -0,0 +1,27 @@
+/*
+name:Faithe Achieng
+reg no:CT101/G/26530
+unit:programming
+description : prompting user to enter items 
+*/
+#include<stdio.h>
+
+int main (){
+int height; 
+printf("enter your height in metres");
+scanf("%d,&height");
+printf("your height is  %d/n height  ");
+
+
+int number ;
+printf("enter your number ");
+scanf("%d,&number");
+printf("your number is%d\n height");
+
+
+int balance;
+printf("enter your balance ");
+scanf("%d,&balance");
+printf("your balance is %d\n balance");
+return 0;
+}
