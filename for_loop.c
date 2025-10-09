75b7ee71562f3a44e10e9e917380a741b39f5e98
Faith415 <faithabuga415@gmail.com>
2025-10-09 08:29:12 -0700

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,14 @@
+/*
+name:Faith
+reg no:CT101/G/26530/25
+programme :for loop
+*/
+#include<stdio.h>
+int main(){
+//for(start;stop;step){}
+int i;
+for(i=100;i>=50;i--){
+printf("%d\n",i);
+}
+return 0;
+}
