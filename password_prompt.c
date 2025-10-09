ed2f5305c989ec018d51ec4ab54f972a5ecadf9c
Faith415 <faithabuga415@gmail.com>
2025-10-09 10:53:15 -0700

    Add files via upload

---
+++ password_prompt.c
@@ -0,0 +1,21 @@
+/*
+name:faith_achieng
+reg no:CT101/G/26530/25
+description:password_prompt
+*/
+#include<stdio.h>
+#include<string.h>
+int main() {
+   char password[20];
+   char correct[]="1234";
+   do {
+       printf("enter password:\n");
+       scanf("%s", &password);
+       if(strcmp(password,correct) !=0) {
+       printf("incorrect password\n");
+       }
+   }while(strcmp(password,correct) !=0);
+   printf("access granded\n");
+   return 0;
+
+}
